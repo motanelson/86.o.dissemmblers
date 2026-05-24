68046fd4645eb992adee6d39d0884cea4c95b00c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-24 19:35:02 +0100

    Add files via upload

--- paint.py
+++ paint.py
@@ -11,9 +11,12 @@ class myapps:
         self.root.title("paint")
         self.root.geometry("640x480")
         self.root.configure(background="black")
-        self.canvas=tk.Canvas(background="black",width=640,height=480)
+        self.canvas=tk.Canvas(background="black",width=640,height=400)
         self.canvas.pack(padx=0,pady=0)
+        self.bt1=tk.Button(background="black",text="delete",command=self.bt1_click,foreground="white")
+        self.bt1.pack(padx=0,pady=0)
         self.counter:int=0
+        self.objs=[]
         self.bb=None
         self.b=self.canvas.bind("<Button-1>",self.clicks)
         self.lastx=0
@@ -23,9 +26,14 @@ class myapps:
         if a==0:
             self.lastx,self.lasty = event.x, event.y
         else:
-            self.canvas.create_line(self.lastx, self.lasty, event.x, event.y, fill="white")
+            self.objs.append(self.canvas.create_line(self.lastx, self.lasty, event.x, event.y, fill="white"))
         
         self.counter=self.counter+1
+    def bt1_click(self):
+        if len(self.objs):
+            a=len(self.objs)-1
+            self.canvas.delete(self.objs[a])
+            self.objs.pop()
 
 
 
